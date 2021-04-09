2a14d1ff6115d497071f4ccb75de9bf59169c457
Liqianqian <liqianqian@huowanes.com>
2021-04-09 18:15:32 +0800

    test-2

--- tests/Testing/TestingPamTest.php
+++ tests/Testing/TestingPamTest.php
@@ -1,13 +1,12 @@
 <?php
 
-namespace Poppy\System\Tests\Setting;
+namespace Poppy\System\Tests\Testing;
 
 /**
  * Copyright (C) Update For IDE
  */
 
 use Poppy\Framework\Application\TestCase;
-use Poppy\System\Tests\Testing\TestingPam;
 
 class TestingPamTest extends TestCase
 {
@@ -17,30 +16,4 @@ class TestingPamTest extends TestCase
         $exclude = TestingPam::exclude();
         $this->assertNotNull($exclude);
     }
-
-
-    public function testGetAllKeys()
-    {
-        $hooks = sys_hook('poppy.system.settings');
-
-        foreach ($hooks as $key => $value) {
-            collect($hooks[$key]['forms'])->map(function ($form) {
-                if (class_exists($form)) {
-                    $objForm = new $form();
-                    if (method_exists($objForm, 'form')) {
-                        $objForm->form();
-                        if (property_exists($objForm, 'group') && property_exists($objForm, 'fields')) {
-                            foreach ($objForm->fields() as $k => $formField) {
-                                if (in_array('required', $formField->getRules(), true) && !sys_setting($objForm->getGroup() . '.' . $formField->column())) {
-                                    dump($formField->column());
-                                }
-                            }
-                        }
-                    }
-                }
-            });
-        }
-    }
-
-
 }
