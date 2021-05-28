f91cf12f70a6d50a5f70acd8d421aca66e72bdab
duoli <zhaody901@126.com>
2021-05-29 01:20:09 +0800

    task # menu 菜单访问简化

--- src/Http/Request/ApiV1/Backend/MenuController.php
+++ src/Http/Request/ApiV1/Backend/MenuController.php
@@ -25,6 +25,36 @@ class MenuController extends BackendController
     {
         $isFullPermission = $this->pam->hasRole(PamRole::BE_ROOT);
         $data             = $this->coreModule()->menus()->withPermission(PamAccount::TYPE_BACKEND, $isFullPermission, $this->pam);
+        $data             = collect($data)->map(function ($module) {
+            $module['key'] = $module['parent'];
+            unset($module['type'], $module['routes'], $module['enabled'], $module['parent']);
+            $groups           = collect($module['groups'])->map(function ($group) {
+                $children = $group['children'] ?? [];
+                if (count($children)) {
+                    $children          = collect($children)->map(function ($child) {
+                        $fnChild           = function ($child) {
+                            if (($child['route'] ?? '') || ($child['url'] ?? '')) {
+                                $child['url'] = $child['url'] ?? route_url($child['route'], $child['route_param'] ?? [], $child['param'] ?? []);
+                            }
+                            unset($child['key'], $child['route_param'], $child['param'], $child['routes']);
+                            return $child;
+                        };
+                        $subChildren       = $child['children']??[];
+                        if ($subChildren) {
+                            $child['children'] = collect($subChildren)->map(function ($child) use ($fnChild) {
+                                return $fnChild($child);
+                            });
+                        }
+                        return $fnChild($child);
+                    })->toArray();
+                    $group['children'] = $children;
+                }
+                unset($group['key'], $group['route_param'], $group['param'], $group['routes']);
+                return $group;
+            });
+            $module['groups'] = $groups;
+            return $module;
+        })->toArray();
         return Resp::success('菜单信息', $data);
     }
 }
